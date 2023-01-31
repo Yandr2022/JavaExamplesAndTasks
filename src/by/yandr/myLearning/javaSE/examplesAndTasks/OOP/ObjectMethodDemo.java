35195b94fdbaf6e14a027abf6db70466b49912bf
Yandr <102684712+Yandr2022@users.noreply.github.com>
2023-01-31 18:02:35 +0300

    added methods of Object Demo

--- src/by/yandr/myLearning/javaSE/examplesAndTasks/OOP/ObjectMethodDemo.java
+++ src/by/yandr/myLearning/javaSE/examplesAndTasks/OOP/ObjectMethodDemo.java
@@ -1,13 +1,61 @@
 package by.yandr.myLearning.javaSE.examplesAndTasks.OOP;
 
 public class ObjectMethodDemo {
+    public static void main(String[] args) {
+        Vehicle vehicle = new Vehicle("car",2,4,130,30);
+
+        Vehicle vehicle1 = new Vehicle("bus");
+        vehicle1.setPassengers(45);
+        vehicle1.setWheels(4);
+        vehicle1.setMaxSpeed(100);
+        vehicle1.setBurnUp(25);
+
+        double time = 0.5;
+        vehicle.showDistanceByTime(time);
+        vehicle1.showDistanceByTime(time);
+
+        time =1.25;
+        System.out.println(String.format("%s will carry %d passengers of %.2f km in %.0f minutes, using %.2f liters of fuel"
+                , vehicle.getName(), vehicle.getPassengers(),vehicle.getDistanceInTime(time),time * 60, vehicle.getFuelConsumptionByTime(time)));
+    }
 
 }
 class Vehicle{
-    int passengers;
-    int wheels;
-    int maxSpeed;
-    int burnUp;
+    private String name;
+    private int passengers;
+    private int wheels;
+    private int maxSpeed;
+    private int burnUp;
+
+    public Vehicle() {
+    }
+
+    public Vehicle(String name) {
+        this.name = name;
+    }
+
+    public Vehicle(String name, int passengers, int wheels, int maxSpeed, int burnUp) {
+        this.name = name;
+        this.passengers = passengers;
+        this.wheels = wheels;
+        this.maxSpeed = maxSpeed;
+        this.burnUp = burnUp;
+    }
+    public Vehicle(Vehicle vehicle) {
+        name = vehicle.getName();
+        passengers = vehicle.getPassengers();
+        wheels = vehicle.getWheels();
+        maxSpeed = vehicle.getMaxSpeed();
+        burnUp = vehicle.getBurnUp();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
     public int getPassengers() {
         return passengers;
@@ -39,5 +87,15 @@ class Vehicle{
 
     public void setBurnUp(int burnUp) {
         this.burnUp = burnUp;
+    }
+
+    void showDistanceByTime(double hours) {
+        System.out.println(String.format("%s travels %.2f km in %.0f minutes", name, hours * maxSpeed, hours * 60));
+    }
+    double getDistanceInTime(double hours) {
+        return hours * maxSpeed;
+    }
+    double getFuelConsumptionByTime(double hours) {
+    return (getDistanceInTime(hours)/100)* burnUp;
     }
 }
